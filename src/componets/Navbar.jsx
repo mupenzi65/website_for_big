31b9c83dd076570d044742adf930906006355132
mupenzi65 <mupenziabdul34@gmail.com>
2023-06-11 22:17:05 +0200

    editing

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { logo,close,menu } from '../assets'
-
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 const Navbar = () => {
     const [toggle,setToggle]=useState(false)
+    const navigation = useNavigate();
   return (
-    <nav className='bg-slate-900 flex items-center header sticky top-0 z-10 '>
+    <nav  className='bg-slate-900 flex items-center header sticky top-0 z-10 container   '>
         <div className='flex flex-[0.5] w-full'>
         <a href=""> <img className='h-[130px] object-cover hover:scale-105' src={logo} alt="big inc" /></a>
 
@@ -14,26 +16,29 @@ const Navbar = () => {
         <div className='  flex-[0.4] justify-center md:block hidden min-w[890p] w-full '>
         <ul className='flex   justify-end items-center list-none text-xl  text-white ' >
             <li className='hover:text-green-400 mr-8 flex whitespace-nowrap'>
-                <a  href="home">Home</a>
+                <NavLink to='/' >Home</NavLink>
+               
             </li>
             <li className='hover:text-green-400 mr-8 whitespace-nowrap'>
-                <a href="">About Us</a>
+            <NavLink to='/' >About Us</NavLink>
+                
             </li>
             <li className='hover:text-green-400 mr-8'>
-                <a href="">Products</a>
+                
+            <NavLink to='/' >Products</NavLink>
             </li>
             <li className='hover:text-green-400 mr-8'>
-                <a href="">Services</a>
+            <NavLink to='/' >Services</NavLink>
             </li>
             <li className='hover:text-green-400 mr-8'>
-                <a href="">Partnership</a>
+            <NavLink to='/' >Partnership</NavLink>
             </li>
         </ul>
 
         </div>
         <div className='hidden md:block w-full flex-[0.1] gap-4 '>
         
-            <a className='' href="contact"><button  className=' flex justify-end  rounded-full   bg-blue-gradient  text-black text-xl px-4 py-4 whitespace-nowrap mr-2 '>Contact Us</button></a>
+            <a className='' href="contact"><button onClick={()=>navigation("/Contact")}  className=' flex justify-end  rounded-full   bg-blue-gradient  text-black text-xl px-4 py-4 whitespace-nowrap mr-2 '>Contact Us</button></a>
         </div>
         <div className='md:hidden flex flex-1 justify-end items-center mr-2 text-white'>
             <img src={toggle ? close:menu} alt="" className='w-[26px]' onClick={()=>setToggle((prev)=>!prev)} />
